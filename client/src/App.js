9e1f1fb1c3fc8d3956473df10c817055c146f387
fallahpour-fr <farzanehhh.fl@gmail.com>
2025-01-23 19:03:16 +0330

    created server side and add basic socker connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './page/home';
+import { useState } from 'react';
+import io from 'socket.io-client';
+
+
+const socket = io.connect('http://localhost:4000');
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
+
   return (
-    <div className="App">
-       Hi
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home 
+            username={username}
+            setUsername={setUsername}
+            room={room}
+            setRoom={setRoom}
+            socket={socket}
+          />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
